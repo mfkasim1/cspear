dfb1baee6a3b43fe0c087549bcfbd8a81a932674
mfkasim91 <firman.kasim@gmail.com>
2019-03-16 12:52:06 +0000

    Including the reduce in main

--- main.cc
+++ main.cc
@@ -7,11 +7,19 @@
 int main() {
   // auto a = csp::array<double>({1,2,3});
   auto a = csp::arange<double>(10);
+  a.reshape({2,5});
+  a += 12;
+  a.rdiv_(2);
+  a = -a;
 
   for (int i = 0; i < a.size(); ++i) {
     std::cout << a.data()[i] << " ";
   }
   std::cout << std::endl;
+
+  std::cout << "max: " << a.max() << std::endl;
+  std::cout << "min: " << a.min() << std::endl;
+  std::cout << "sum: " << a.sum() << std::endl;
 
   return 0;
 }
