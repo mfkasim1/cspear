917f885c986b12a46e3715c094480895fd868cb6
mfkasim91 <firman.kasim@gmail.com>
2019-03-19 09:26:01 +0000

    Contiguous view and elementwise iterator

---
+++ include/cspear/iterators/ewise-iterator.h
@@ -0,0 +1,51 @@
+#ifndef CSPEAR_ITERATORS_EWISE_ITERATOR_H
+#define CSPEAR_ITERATORS_EWISE_ITERATOR_H
+
+#include <cspear/views/contiguous-view.h>
+
+namespace csp {
+  template <typename T, typename I, typename View>
+  class EWiseIterator {
+    public:
+    // constructor
+    EWiseIterator(T* data, const View<I>& view);
+
+    // iterator operator
+    T& operator*();
+    EWiseIterator& operator++();
+    bool isvalid();
+  };
+
+  // partial template specialization for different views
+  template <typename T, typename I>
+  class EWiseIterator<T,I,ContiguousView> {
+    I sz_;
+    T* data_;
+    I offset_;
+
+    public:
+    // constructor
+    EWiseIterator(T* data, const ContiguousView<I>& view) {
+      sz_ = view.size();
+      data_ = data;
+      offset_ = 0;
+    }
+
+    // iterator operator
+    inline T& operator*() {
+      return *data;
+    }
+
+    inline EWiseIterator& operator++() {
+      data_++;
+      offset_++;
+      return *this;
+    }
+
+    inline bool isvalid() {
+      return offset_ < sz_;
+    }
+  };
+}
+
+#endif
---
+++ include/cspear/views/contiguous-view.h
@@ -0,0 +1,68 @@
+#ifndef CSPEAR_VIEWS_CONTIGUOUS_VIEW_H
+#define CSPEAR_VIEWS_CONTIGUOUS_VIEW_H
+
+#include <vector>
+
+namespace csp {
+  template <typename I>
+  class ContiguousView {
+    std::vector<I> shape_;
+    I sz_;
+
+    public:
+    ContiguousView(const std::vector<I>& shape);
+    ContiguousView(std::initializer_list<I> shape);
+
+    void reshape(const std::vector<I>& shape);
+    void reshape(std::initializer_list<I> shape);
+    I size() const;
+    const std::vector<I>& shape() const;
+
+    private:
+    static I _compute_size(const std::vector<I>& shape);
+  };
+
+  // implementations
+  template <typename I>
+  ContiguousView<I>::ContiguousView(const std::vector<I>& shape) {
+    reshape(shape);
+  }
+
+  template <typename I>
+  ContiguousView<I>::ContiguousView(std::initializer_list<I> shape) {
+    reshape(shape);
+  }
+
+  template <typename I>
+  inline void ContiguousView<I>::reshape(const std::vector<I>& shape) {
+    shape_ = shape;
+    sz_ = _compute_size(shape_);
+  }
+
+  template <typename I>
+  inline void ContiguousView<I>::reshape(std::initializer_list<I> shape) {
+    shape_ = shape;
+    sz_ = _compute_size(shape_);
+  }
+
+  template <typename I>
+  inline I size() const {
+    return sz_;
+  }
+
+  template <typename I>
+  inline const std::vector<I>& shape() const {
+    return shape_;
+  }
+
+  template <typename I>
+  static inline I _compute_size(const std::vector<I>& shape) {
+    I sz = 1;
+    for (auto& s : shape) {
+      sz *= s;
+    }
+    return sz;
+  }
+}
+
+#endif
