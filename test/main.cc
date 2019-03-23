d2135ee226ca72897591feb2b61aea8b42a4ba7e
mfkasim91 <firman.kasim@gmail.com>
2019-03-23 13:07:17 +0000

    Renamed the folder array to test-array

--- test/main.cc
+++ test/main.cc
@@ -2,11 +2,11 @@
 #define CSPEAR_TEST_MAIN_CC
 
 #include "gtest/gtest.h"
-#include "array/test-init.h"
-#include "array/test-reshape.h"
-#include "array/test-reduce.h"
-#include "array/test-op.h"
-#include "array/test-bcast.h"
+#include "test-array/test-init.h"
+#include "test-array/test-reshape.h"
+#include "test-array/test-reduce.h"
+#include "test-array/test-op.h"
+#include "test-array/test-bcast.h"
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
