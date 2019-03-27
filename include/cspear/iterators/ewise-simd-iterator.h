c705d5f37841c4a69bc4e2b86477c1219b6341ee
mfkasim91 <firman.kasim@gmail.com>
2019-03-27 16:35:43 +0000

    EWiseSIMDIterator

---
+++ include/cspear/iterators/ewise-simd-iterator.h
@@ -0,0 +1,71 @@
+#ifndef CSPEAR_ITERATORS_EWISE_SIMD_ITERATOR_H
+#define CSPEAR_ITERATORS_EWISE_SIMD_ITERATOR_H
+
+#include <cspear/views/contiguous-view.h>
+#include <cspear/views/filter-view.h>
+#include <cspear/simd/vector.h>
+
+namespace csp {
+  template <typename T, typename I, typename View>
+  class EWiseSIMDIterator {
+    public:
+    static const bool is_implemented = false;
+
+    // constructor
+    EWiseSIMDIterator(T* data, const View& view);
+
+    // iterator operator
+    simd::Vector<T>& load(); // load the memory data into the intrinsic and return the intrinsic
+    EWiseIterator& operator++();
+    void store(); // store the intrinsic value to the memory data
+    operator bool() const;
+  };
+
+  // partial template specialization for different views
+  template <typename T, typename I>
+  class EWiseSIMDIterator<T,I,ContiguousView<I> > {
+    I sz_;
+    T* data_;
+    simd::Vector<T> vec_;
+    I simd_sz_;
+    I offset_;
+
+    public:
+    static const bool is_implemented = true;
+
+    // constructor
+    EWiseSIMDIterator(T* data, const ContiguousView<I>& view) {
+      sz_ = view.size();
+      data_ = data;
+      offset_ = 0;
+      simd_sz_ = vec_.size();
+    }
+
+    // iterator operator
+    inline simd::Vector<T>& load() {
+      if (sz_ >= offset_ + simd_sz_)
+        return vec_.load(data_);
+      else
+        return vec_.partial_load(data_, sz_-offset_);
+    }
+
+    inline EWiseIterator& operator++() {
+      data_ += simd_sz_;
+      offset_ += simd_sz_;
+      return *this;
+    }
+
+    inline void store() {
+      if (sz_ >= offset_ + simd_sz_)
+        vec_.store(data_);
+      else
+        vec_.partial_store(data_, sz_-offset_);
+    }
+
+    inline operator bool() const {
+      return offset_ < sz_;
+    }
+  };
+}
+
+#endif
--- include/cspear/simd/vector.h
+++ include/cspear/simd/vector.h
@@ -1,14 +1,26 @@
 #ifndef CSPEAR_SIMD_TYPES_H
 #define CSPEAR_SIMD_TYPES_H
 
-#if defined(__AVX2__)
+#if defined(__AVX512__)
   #include <cspear/simd/vector-avx.h>
-  #define __SIMD__ 1
-  #define __SIMD_AVX2__
-#elif defined(__AVX__)
-  #include <cspear/simd/vector-avx.h>
-  #define __SIMD__ 1
-  #define __SIMD_AVX__
+  #define __SIMD__
 #endif
+
+#if defined(__AVX__)
+  #include <cspear/simd/vector-avx.h>
+  #define __SIMD__
+#endif
+
+namespace csp {
+  namespace simd {
+    #if defined(__AVX512__)
+      #define __SIMD_AVX512__
+      template <typename T> using Vector = VectorAVX512<T>;
+    #elif defined(__AVX__)
+      #define __SIMD_AVX__
+      template <typename T> using Vector = VectorAVX<T>;
+    #endif
+  }
+}
 
 #endif
