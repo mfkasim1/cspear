951f661d03ecb16d9ac7abd588f5116a23e506a0
mfkasim91 <firman.kasim@gmail.com>
2019-04-02 17:20:40 +0100

    Test all types on reshape

--- test/test-array/test-reshape.h
+++ test/test-array/test-reshape.h
@@ -9,10 +9,10 @@ namespace {
   template <typename T>
   class ReshapeTest : public testing::Test {};
 
-  TYPED_TEST_SUITE(ReshapeTest, RealNumbers);
+  TYPED_TEST_SUITE(ReshapeTest, AllTypes);
 
   TYPED_TEST(ReshapeTest,Resize) {
-    csp::array<TypeParam> arr = {(TypeParam)1, (TypeParam)2};
+    csp::array<TypeParam> arr = {(TypeParam)1, (TypeParam)0};
     arr.resize_(5);
 
     EXPECT_EQ(arr.size(), 5);
@@ -23,8 +23,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,Reshape1) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     arr.reshape_({3,2});
 
     EXPECT_EQ(arr.size(), 6);
@@ -36,8 +36,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,Reshape2) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     std::vector<int> sh = {3,2};
     arr.reshape_(sh);
 
@@ -50,8 +50,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,Squeeze) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     arr.reshape_({3,1,2,1});
 
     EXPECT_EQ(arr.size(), 6);
@@ -70,8 +70,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,ExpandDims) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     arr.reshape_({3,2,1});
 
     EXPECT_EQ(arr.size(), 6);
@@ -89,8 +89,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,Ravel) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     arr.reshape_({3,2,1});
 
     EXPECT_EQ(arr.size(), 6);
@@ -105,8 +105,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,ReshapeError1) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     try {
       arr.reshape_({3,3});
       FAIL() << "Expected runtime_error with unmatched reshape.";
@@ -125,8 +125,8 @@ namespace {
 
   TYPED_TEST(ReshapeTest,ReshapeError2) {
     csp::array<TypeParam> arr = {
-      (TypeParam)1, (TypeParam)2, (TypeParam)3,
-      (TypeParam)4, (TypeParam)5, (TypeParam)6};
+      (TypeParam)1, (TypeParam)2, (TypeParam)0,
+      (TypeParam)4, (TypeParam)5, (TypeParam)0};
     std::vector<int> shape = {3,3};
     try {
       arr.reshape_(shape);
