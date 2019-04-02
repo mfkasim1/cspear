5063dec8d662cffce5187e271b60c693220f7604
mfkasim91 <firman.kasim@gmail.com>
2019-04-02 17:54:48 +0100

    Test integers on filter view

--- test/test-view/test-filter-view.h
+++ test/test-view/test-filter-view.h
@@ -10,90 +10,97 @@ namespace {
   template <typename T>
   class FilterView : public testing::Test {};
 
-  TYPED_TEST_SUITE(FilterView, RealNumbers);
+  TYPED_TEST_SUITE(FilterView, AllNumbers);
 
   TYPED_TEST(FilterView,FromContiguous) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    auto b = a.filter(a < 3.1);
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    auto b = a.filter(a <= (TypeParam)3);
 
     std::vector<int> shape = {3};
     EXPECT_EQ(b.size(), 3);
     EXPECT_EQ(b.shape(), shape);
-    EXPECT_NEAR(b[0], 1.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], 2.1, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)1, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)2, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,InplaceAddAValue) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    a.filter(a < 3.1) += 2.0;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    a.filter(a <= (TypeParam)3) += (TypeParam)2;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(a.size(), 6);
     EXPECT_EQ(a.shape(), shape);
-    EXPECT_NEAR(a[0], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[1], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[2], 5.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[3], 6.9, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[4], 4.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[5], 5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[0], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[2], (TypeParam)5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[3], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[4], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,InplaceAddArray) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    csp::array<TypeParam> b = {2.0, 6.0, 4.2};
-    a.filter(a < 3.1) += b;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    csp::array<TypeParam> b = {(TypeParam)2, (TypeParam)6, (TypeParam)4};
+    a.filter(a <= (TypeParam)3) += b;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(a.size(), 6);
     EXPECT_EQ(a.shape(), shape);
-    EXPECT_NEAR(a[0], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[1], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[2], 9.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[3], 6.9, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[4], 6.3, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[5], 5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[0], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[2], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[3], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[4], (TypeParam)6, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,InplaceClip) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    a.filter(a < 3.1).clip_lb_(2.5);
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    a.filter(a <= (TypeParam)3).clip_lb_((TypeParam)2);
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(a.size(), 6);
     EXPECT_EQ(a.shape(), shape);
-    EXPECT_NEAR(a[0], 2.5, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[1], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[2], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[3], 6.9, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[4], 2.5, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[5], 5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[2], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[3], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[4], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,AddAValue) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    auto c = a.filter(a < 3.1) + 2.0;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    auto c = a.filter(a <= (TypeParam)3) + (TypeParam)2;
 
     std::vector<int> shape = {3};
     EXPECT_EQ(c.size(), 3);
     EXPECT_EQ(c.shape(), shape);
-    EXPECT_NEAR(c[0], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(c[1], 5.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(c[2], 4.1, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[0], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[1], (TypeParam)5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[2], (TypeParam)4, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,AddArray) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    csp::array<TypeParam> b = {2.0, 6.0, 4.2};
-    auto c = a.filter(a < 3.1) + b;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    csp::array<TypeParam> b = {(TypeParam)2, (TypeParam)6, (TypeParam)4};
+    auto c = a.filter(a <= (TypeParam)3) + b;
 
     std::vector<int> shape = {3};
     EXPECT_EQ(c.size(), 3);
     EXPECT_EQ(c.shape(), shape);
-    EXPECT_NEAR(c[0], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(c[1], 9.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(c[2], 6.3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[0], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[1], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(c[2], (TypeParam)6, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,MismatchShape) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    csp::array<TypeParam> b = {2.0, 6.0, 4.2};
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    csp::array<TypeParam> b = {(TypeParam)2, (TypeParam)6, (TypeParam)4};
     try {
-      auto c = a.filter(b > 2.1);
+      auto c = a.filter(b >= (TypeParam)2);
       FAIL() << "A runtime error should be thrown with mismatched filter shape.";
     }
     catch (const std::runtime_error& e) {
@@ -104,33 +111,35 @@ namespace {
     }
   }
   TYPED_TEST(FilterView,Assign) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    csp::array<TypeParam> b = {2.0, 6.0, 4.2};
-    a.filter(a < 3.1) = b;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    csp::array<TypeParam> b = {(TypeParam)2, (TypeParam)6, (TypeParam)4};
+    a.filter(a <= (TypeParam)3) = b;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(a.size(), 6);
     EXPECT_EQ(a.shape(), shape);
-    EXPECT_NEAR(a[0], 2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[1], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[2], 6.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[3], 6.9, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[4], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[5], 5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[2], (TypeParam)6, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[3], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[4], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(FilterView,InplaceAddAValueWithNoTrue) {
-    csp::array<TypeParam> a = {{1.0, 4.2, 3.0}, {6.9, 2.1, 5.2}};
-    a.filter(a < -3.1) += 2.0;
+    csp::array<TypeParam> a = {{(TypeParam)1, (TypeParam)4, (TypeParam)3},
+                               {(TypeParam)7, (TypeParam)2, (TypeParam)5}};
+    a.filter(a <= (TypeParam)0) += (TypeParam)2;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(a.size(), 6);
     EXPECT_EQ(a.shape(), shape);
-    EXPECT_NEAR(a[0], 1.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[1], 4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[2], 3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[3], 6.9, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[4], 2.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(a[5], 5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[0], (TypeParam)1, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[2], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[3], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[4], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(a[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
 }
 
