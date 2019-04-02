0d5c7d67b988f511e08b3e91302f6e613a50afcc
mfkasim91 <firman.kasim@gmail.com>
2019-04-02 17:17:10 +0100

    Test integers on broadcast

--- test/test-array/test-bcast.h
+++ test/test-array/test-bcast.h
@@ -9,71 +9,72 @@
 namespace {
   template <typename T>
   class BCastOp : public testing::Test {};
-  template <typename T>
-  class BCastOpCMP : public testing::Test {};
 
-  TYPED_TEST_SUITE(BCastOp, RealNumbers);
-  TYPED_TEST_SUITE(BCastOpCMP, RealNumbers);
+  TYPED_TEST_SUITE(BCastOp, AllNumbers);
 
   // broadcast test for the shapes
   TYPED_TEST(BCastOp,Add1) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}, {8.2, 7.1, -4.5}};
-    csp::array<TypeParam> ars = {1.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3},
+                                 {(TypeParam)8, (TypeParam)7, (TypeParam)4}};
+    csp::array<TypeParam> ars = {(TypeParam)1};
     auto b = arr + ars;
 
     EXPECT_EQ(b.shape(), arr.shape());
-    EXPECT_NEAR(b[0], (TypeParam)2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)3.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)9.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)8.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)-3.5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)8, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Add2) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}, {8.2, 7.1, -4.5}};
-    csp::array<TypeParam> ars = {1.0, 2.0, 4.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3},
+                                 {(TypeParam)8, (TypeParam)7, (TypeParam)4}};
+    csp::array<TypeParam> ars = {(TypeParam)1, (TypeParam)2, (TypeParam)4};
     auto b = arr + ars;
 
     EXPECT_EQ(b.shape(), arr.shape());
-    EXPECT_NEAR(b[0], (TypeParam)2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)4.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)7.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)9.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)9.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)-0.5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)7, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)8, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Add3) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}, {8.2, 7.1, -4.5}};
-    csp::array<TypeParam> ars = {1.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3},
+                                 {(TypeParam)8, (TypeParam)7, (TypeParam)4}};
+    csp::array<TypeParam> ars = {(TypeParam)1, (TypeParam)2};
     ars.expand_dims_(1);
     auto b = arr + ars;
 
     EXPECT_EQ(b.shape(), arr.shape());
-    EXPECT_NEAR(b[0], (TypeParam)2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)3.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)10.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)9.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)-2.5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)10, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)6, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Add4) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}};
-    csp::array<TypeParam> ars = {1.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3}};
+    csp::array<TypeParam> ars = {(TypeParam)1, (TypeParam)2};
     ars.expand_dims_(1);
     auto b = arr + ars;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(b.shape(), shape);
-    EXPECT_NEAR(b[0], (TypeParam)2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)3.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)4.1, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)5.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)5, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Add5) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}, {3.1, 3.0, 2.1}};
-    csp::array<TypeParam> ars = {1.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3},
+                                 {(TypeParam)8, (TypeParam)7, (TypeParam)4}};
+    csp::array<TypeParam> ars = {(TypeParam)1, (TypeParam)2};
     try {
       auto b = arr + ars;
       FAIL() << "A runtime error should be thrown if the dimension is mismatch.";
@@ -88,53 +89,53 @@ namespace {
 
   // broadcast test with the operators
   TYPED_TEST(BCastOp,Sub) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.2}};
-    csp::array<TypeParam> ars = {1.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)1, (TypeParam)2};
     ars.expand_dims_(1);
     auto b = arr - ars;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(b.shape(), shape);
-    EXPECT_NEAR(b[0], (TypeParam)0.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)1.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)2.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)-1.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)0.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)1.2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)19, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)29, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)8, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)18, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)28, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Mult) {
-    csp::array<TypeParam> arr = {{1.0, 2.1, 3.2}};
-    csp::array<TypeParam> ars = {3.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)1, (TypeParam)2, (TypeParam)3}};
+    csp::array<TypeParam> ars = {(TypeParam)3, (TypeParam)2};
     ars.expand_dims_(1);
     auto b = arr * ars;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(b.shape(), shape);
-    EXPECT_NEAR(b[0], (TypeParam)3.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)6.3, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)9.6, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)2.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)4.2, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)6.4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)3, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)6, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)9, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)2, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)4, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)6, AbsTol<TypeParam>::val);
   }
   TYPED_TEST(BCastOp,Div) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {4.0, 2.0};
+    csp::array<TypeParam> arr = {{(TypeParam)40, (TypeParam)20, (TypeParam)60}};
+    csp::array<TypeParam> ars = {(TypeParam)4, (TypeParam)2};
     ars.expand_dims_(1);
     auto b = arr / ars;
 
     std::vector<int> shape = {2,3};
     EXPECT_EQ(b.shape(), shape);
-    EXPECT_NEAR(b[0], (TypeParam)0.25, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[1], (TypeParam)0.5, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[2], (TypeParam)0.75, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[3], (TypeParam)0.5, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[4], (TypeParam)1.0, AbsTol<TypeParam>::val);
-    EXPECT_NEAR(b[5], (TypeParam)1.5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[0], (TypeParam)10, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[1], (TypeParam)5, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[2], (TypeParam)15, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[3], (TypeParam)20, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[4], (TypeParam)10, AbsTol<TypeParam>::val);
+    EXPECT_NEAR(b[5], (TypeParam)30, AbsTol<TypeParam>::val);
   }
-  TYPED_TEST(BCastOpCMP,Gt) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,Gt) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr > ars;
 
@@ -147,9 +148,9 @@ namespace {
     EXPECT_EQ(b[4], false);
     EXPECT_EQ(b[5], true);
   }
-  TYPED_TEST(BCastOpCMP,GtEq) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,GtEq) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr >= ars;
 
@@ -162,9 +163,9 @@ namespace {
     EXPECT_EQ(b[4], true);
     EXPECT_EQ(b[5], true);
   }
-  TYPED_TEST(BCastOpCMP,Lt) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,Lt) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr < ars;
 
@@ -177,9 +178,9 @@ namespace {
     EXPECT_EQ(b[4], false);
     EXPECT_EQ(b[5], false);
   }
-  TYPED_TEST(BCastOpCMP,LtEq) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,LtEq) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr <= ars;
 
@@ -192,9 +193,9 @@ namespace {
     EXPECT_EQ(b[4], true);
     EXPECT_EQ(b[5], false);
   }
-  TYPED_TEST(BCastOpCMP,Eq) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,Eq) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr == ars;
 
@@ -207,9 +208,9 @@ namespace {
     EXPECT_EQ(b[4], true);
     EXPECT_EQ(b[5], false);
   }
-  TYPED_TEST(BCastOpCMP,NEq) {
-    csp::array<TypeParam> arr = {{1.0, 2.0, 3.0}};
-    csp::array<TypeParam> ars = {1.5, 2.0};
+  TYPED_TEST(BCastOp,NEq) {
+    csp::array<TypeParam> arr = {{(TypeParam)10, (TypeParam)20, (TypeParam)30}};
+    csp::array<TypeParam> ars = {(TypeParam)15, (TypeParam)20};
     ars.expand_dims_(1);
     auto b = arr != ars;
 
