b8cb309457883432859ba4f22b5cced20fd96cc2
mfkasim91 <firman.kasim@gmail.com>
2019-04-04 09:06:47 +0100

    More math operators

--- include/cspear/array/ufuncs-math.h
+++ include/cspear/array/ufuncs-math.h
@@ -12,12 +12,21 @@ This is csp::array wrapper for cmath operations.
 */
 
 #define CSPEAR_MATH_UFUNC_UNARY(op) \
-template <typename T> \
-struct op { \
-  static const bool has_simd = false; \
-  static inline T unary(const T& a) { return std::op(a); } \
-  static inline simd::Vector<T> simd_unary(const simd::Vector<T>& a) {} \
-};
+  template <typename T> \
+  struct op { \
+    static const bool has_simd = false; \
+    static inline T unary(const T& a) { return std::op(a); } \
+    static inline simd::Vector<T> simd_unary(const simd::Vector<T>& a) {} \
+  };
+
+#define CSPEAR_MATH_UFUNC_BINARY(op) \
+  template <typename T> \
+  struct op { \
+    static const bool has_simd = false; \
+    static inline T binary(const T& a, const T& b) { return std::op(a, b); } \
+    static inline simd::Vector<T> simd_binary( \
+        const simd::Vector<T>& a, const simd::Vector<T>& b) {} \
+  };
 
 namespace csp {
   namespace ufunc {
@@ -28,9 +37,13 @@ namespace csp {
     CSPEAR_MATH_UFUNC_UNARY(acos);
     CSPEAR_MATH_UFUNC_UNARY(asin);
     CSPEAR_MATH_UFUNC_UNARY(atan);
+    CSPEAR_MATH_UFUNC_BINARY(atan2);
     CSPEAR_MATH_UFUNC_UNARY(cosh);
     CSPEAR_MATH_UFUNC_UNARY(sinh);
     CSPEAR_MATH_UFUNC_UNARY(tanh);
+    CSPEAR_MATH_UFUNC_UNARY(acosh);
+    CSPEAR_MATH_UFUNC_UNARY(asinh);
+    CSPEAR_MATH_UFUNC_UNARY(atanh);
     CSPEAR_MATH_UFUNC_UNARY(exp);
     CSPEAR_MATH_UFUNC_UNARY(log);
     CSPEAR_MATH_UFUNC_UNARY(log10);
@@ -38,13 +51,16 @@ namespace csp {
     CSPEAR_MATH_UFUNC_UNARY(expm1);
     CSPEAR_MATH_UFUNC_UNARY(log1p);
     CSPEAR_MATH_UFUNC_UNARY(log2);
+    CSPEAR_MATH_UFUNC_BINARY(pow);
     CSPEAR_MATH_UFUNC_UNARY(sqrt);
     CSPEAR_MATH_UFUNC_UNARY(cbrt);
+    CSPEAR_MATH_UFUNC_BINARY(hypot);
     CSPEAR_MATH_UFUNC_UNARY(erf);
     CSPEAR_MATH_UFUNC_UNARY(erfc);
     CSPEAR_MATH_UFUNC_UNARY(tgamma);
     CSPEAR_MATH_UFUNC_UNARY(lgamma);
     CSPEAR_MATH_UFUNC_UNARY(ceil);
+    CSPEAR_MATH_UFUNC_UNARY(trunc);
     CSPEAR_MATH_UFUNC_UNARY(floor);
     CSPEAR_MATH_UFUNC_UNARY(round);
     CSPEAR_MATH_UFUNC_UNARY(abs);
